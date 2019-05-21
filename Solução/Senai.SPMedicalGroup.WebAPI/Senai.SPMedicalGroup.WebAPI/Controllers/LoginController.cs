36fe38b5b5285a02ccf13fd0c0dc7f77e9e45c3b
Ariel Paixão Dos Santos <33747821839@info.local>
2019-05-21 17:24:17 -0300

    nomeUsuario

--- Solução/Senai.SPMedicalGroup.WebAPI/Senai.SPMedicalGroup.WebAPI/Controllers/LoginController.cs
+++ Solução/Senai.SPMedicalGroup.WebAPI/Senai.SPMedicalGroup.WebAPI/Controllers/LoginController.cs
@@ -42,7 +42,8 @@ namespace Senai.SPMedicalGroup.WebAPI.Controllers
                     new Claim(JwtRegisteredClaimNames.Email, usuarioProcurado.Email),
                     new Claim(JwtRegisteredClaimNames.Jti, usuarioProcurado.Id.ToString()),
                     new Claim(ClaimTypes.Role, usuarioProcurado.IdTipoUsuarioNavigation.Nome),
-                    new Claim("tipoUsuario", usuarioProcurado.IdTipoUsuarioNavigation.Nome)
+                    new Claim("tipoUsuario", usuarioProcurado.IdTipoUsuarioNavigation.Nome),
+                    new Claim("nomeUsuario", usuarioProcurado.Nome)
                 };
 
                 var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("spmedgroup-chave-autenticacao"));
